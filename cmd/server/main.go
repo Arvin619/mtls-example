204ad545a309a4fee1ff6435af54428500c7ce91
Arvin619 <wasd572@gmail.com>
2025-03-23 17:46:28 +0800

    refactor: move mTLS verification from stdlib TLS to middleware
    - Replaced stdlib TLS client certificate verification with custom
    `mTLSMiddleware`
    - TLS config changed to `RequestClientCert` to allow middleware-based
    verification
    - Ensured that client behavior remains unchanged while improving
    flexibility

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,14 +13,36 @@ const (
 	port = 8443
 )
 
-func mTLSMiddleware(next http.Handler) http.Handler {
+func mTLSMiddleware(clientCAs *x509.CertPool, verifyPeerCertificate func(*x509.Certificate) error, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.TLS == nil || len(r.TLS.PeerCertificates) == 0 {
 			http.Error(w, "required client certificate", http.StatusUnauthorized)
 			return
 		}
 
-		fmt.Println(r.TLS.PeerCertificates[0].Subject.CommonName)
+		opts := x509.VerifyOptions{
+			Intermediates: x509.NewCertPool(),
+			Roots:         clientCAs,
+			KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		}
+
+		for _, cert := range r.TLS.PeerCertificates[1:] {
+			opts.Intermediates.AddCert(cert)
+		}
+
+		cert := r.TLS.PeerCertificates[0]
+
+		if _, err := cert.Verify(opts); err != nil {
+			http.Error(w, "failed to verify certificate: "+err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		if verifyPeerCertificate != nil {
+			if err := verifyPeerCertificate(cert); err != nil {
+				http.Error(w, "failed to verify peer certificate: "+err.Error(), http.StatusUnauthorized)
+				return
+			}
+		}
 
 		next.ServeHTTP(w, r)
 	})
@@ -32,21 +54,21 @@ func ping(w http.ResponseWriter, _ *http.Request) {
 }
 
 func main() {
-	handler := http.NewServeMux()
-	handler.Handle("GET /ping", mTLSMiddleware(http.HandlerFunc(ping)))
-
 	clientCaCertPool := x509.NewCertPool()
 	clientCaCertPem, err := os.ReadFile("./cert/client-ca.pem")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if !clientCaCertPool.AppendCertsFromPEM(clientCaCertPem) {
 		log.Fatal("failed to append certs from pem")
 	}
 
+	handler := http.NewServeMux()
+	handler.Handle("GET /ping", mTLSMiddleware(clientCaCertPool, nil, http.HandlerFunc(ping)))
+
 	tlsCfg := &tls.Config{
-		ClientCAs:  clientCaCertPool,
-		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientAuth: tls.RequestClientCert,
 	}
 
 	srv := http.Server{
